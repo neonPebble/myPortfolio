e9eace344387e78fa685bae21aa6ef27ef20cfd4
neonPebble <akashparhi0@gmail.com>
2022-03-27 07:35:56 +0530

    added gradient background

--- src/pages/home/components/ProjectCard.jsx
+++ src/pages/home/components/ProjectCard.jsx
@@ -4,14 +4,26 @@ import home1 from './project-pics/home1.png'
 
 export default function ProjectCard() {
   return (
-    <div className=" bg-[#ffffff30] p-[20px] rounded-xl seeThroughCard shadow-xl grid grid-rows-[auto,auto,auto] projectCard">
-      <img alt="Project Name" src={home1} className="w-full h-auto" />
+    <div className=" bg-[#ffffff30] p-[20px] rounded-xl seeThroughCard shadow-xl grid grid-rows-[auto,auto,auto,auto] projectCard">
+      <img
+        alt="Project Name"
+        src={home1}
+        className="w-full h-auto rounded-xl"
+      />
       <h2 className="text-[#e5e6ae] text-center p-[10px] font-semibold tracking-wider py-2">
         AnimeList
       </h2>
       <p className="py-2 text-center">
         A fullstack app to lookup and save animes and notes
       </p>
+      <div className="flex items-center justify-center gap-2">
+        <button type="button" className="px-4 py-1 text-center rounded-full">
+          Code
+        </button>
+        <button type="button" className="px-4 py-1 text-center rounded-full">
+          Live
+        </button>
+      </div>
     </div>
   )
 }
--- src/pages/home/components/ProjectCard2.jsx
+++ src/pages/home/components/ProjectCard2.jsx
@@ -5,13 +5,25 @@ import home1 from './project-pics/home1.png'
 export default function ProjectCard2() {
   return (
     <div className=" bg-[#ffffff30] p-[20px] rounded-xl seeThroughCard shadow-xl flex flex-col justify-center items-center projectCard">
-      <img alt="Project Name" src={home1} className="w-full h-auto " />
+      <img
+        alt="Project Name"
+        src={home1}
+        className="w-full h-auto rounded-xl"
+      />
       <h2 className="text-[#e5e6ae] text-center p-[10px] font-semibold tracking-wider py-2">
         AnimeList
       </h2>
       <p className="py-2 text-center">
         A fullstack app to lookup and save animes and notes
       </p>
+      <div className="flex items-center justify-center gap-2">
+        <button type="button" className="px-4 py-1 text-center rounded-full">
+          Code
+        </button>
+        <button type="button" className="px-4 py-1 text-center rounded-full">
+          Live
+        </button>
+      </div>
     </div>
   )
 }

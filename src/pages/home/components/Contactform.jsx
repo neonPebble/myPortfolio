708964e766be971c1851742e8c9ea4b3ba67ce9b
neonPebble <akashparhi0@gmail.com>
2022-03-29 19:12:02 +0530

    changed style in mediaquery

--- src/pages/home/components/Contactform.jsx
+++ src/pages/home/components/Contactform.jsx
@@ -1,23 +1,96 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 export default function Contactform() {
   const [emailInput, setEmailInput] = useState('')
   const [nameInput, setNameInput] = useState('')
   const [messageInput, setMessageInput] = useState('')
 
+  const mailapiUrl = '/sendmail'
+
+  async function sendMail(e) {
+    e.target.classList.add('btnAnimate')
+
+    const userMailData = {
+      senderEmail: emailInput,
+      message: messageInput,
+      senderName: nameInput
+    }
+
+    setTimeout(async () => {
+      e.target.classList.remove('btnAnimate')
+
+      console.log('the seyTimeout callback is working')
+
+      const resObj = await fetch(mailapiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify(userMailData)
+      })
+
+      const resJson = await resObj.json()
+      if (resJson.successful) {
+        console.log('success')
+      } else {
+        console.log('failed to send')
+      }
+    }, 500)
+  }
+
+  function clearIput(e) {
+    e.target.classList.add('btnAnimate')
+    setEmailInput('')
+    setNameInput('')
+    setMessageInput('')
+    setTimeout(() => {
+      e.target.classList.remove('btnAnimate')
+    }, 500)
+  }
   return (
-    <div>
-      <div>
-        <input type="text" placeholder="name" value={nameInput} />
-        <input type="email" placeholder="email" value={emailInput} />
+    <div className="p-2 w-min">
+      <div className="flex w-full gap-2 contactInputContainer">
+        <input
+          type="text"
+          placeholder="name"
+          className="bg-[#5d064f] my-1 tracking-wider"
+          value={nameInput}
+          onChange={e => setNameInput(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          value={emailInput}
+          onChange={e => setEmailInput(e.target.value)}
+          className="bg-[#5d064f] my-1 tracking-wider"
+        />
       </div>
-      <textarea
-        className="message"
-        placeholder="message"
-        rows="5"
-        cols="36"
-        value={messageInput}
-      />
+      <div>
+        <textarea
+          className="message bg-[#5d064f] my-2 w-full tracking-wider"
+          placeholder="message"
+          value={messageInput}
+          rows="5"
+          onChange={e => setMessageInput(e.target.value)}
+        />
+      </div>
+      <div className="flex items-center justify-center gap-2 ">
+        <button
+          type="button"
+          className="px-4 py-1 text-center rounded-full fiveGradBtn"
+          onClick={sendMail}
+        >
+          Send
+        </button>
+        <button
+          type="button"
+          className="px-4 py-1 text-center rounded-full fiveGradBtn"
+          onClick={clearIput}
+        >
+          clear
+        </button>
+      </div>
     </div>
   )
 }
